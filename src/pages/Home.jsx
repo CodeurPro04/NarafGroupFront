8024a4583a86e25de9fc3491115040caf0c692aa
yasmine1110 <yasminemariamd@gmail.com>
2026-01-14 23:23:34 +0100

    les erreurs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -151,7 +151,7 @@ const Home = () => {
     <div className="bg-white">
       <Hero />
       {/* Services Section */}
-      <section className="py-20 bg-white">
+      <section className="py-20 bg-white ">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-16">
             <h2 className="text-4xl font-bold text-gray-900 mb-4">
